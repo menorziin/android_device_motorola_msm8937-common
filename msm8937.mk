d83ce78b3467c3c981d01f8c27f058e301a5effc
Deivid21 <david.parra.ignacio@gmail.com>
2020-12-27 23:12:40 -0300

    msm8937-common: drop aosp fm radio
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -113,15 +113,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
-PRODUCT_BOOT_JARS += \
-    qcom.fmradio
-
 # Filesystem
 PRODUCT_PACKAGES += \
     fs_config_files
